5c31e3d1630558ec9289c2061ef914eb14d18848
anviparikh9 <parikh9@uwindsor.ca>
2021-10-12 20:10:13 +0530

    citycight : initial commit

---
+++ src/test/java/com/citysight/CitysightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citysight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitysightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
